bf029ba7046f793c566c3c5fd1070a89d7edbaaa
Fabricio Murillo <fabriciomurillo24@gmail.com>
2020-11-11 23:23:50 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '21fa70d6a4d0177894766be588df2e9b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4888b07c-f6c6-4c7b-97fe-6848cdb3b352'
+};
