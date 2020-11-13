b5b7528abdeb3c85490d6a4056e014a12c52a71d
Fabricio Murillo <fabriciomurillo24@gmail.com>
2020-11-13 17:21:44 -0600

    :rocket: Relation between Currencies

--- config/database.js
+++ config/database.js
@@ -1,19 +1,39 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host: env("DATABASE_HOST", "127.0.0.1"),
+            port: env.int("DATABASE_PORT", 27017),
+            database: env("DATABASE_NAME", "strapi"),
+            username: env("DATABASE_USERNAME", ""),
+            password: env("DATABASE_PASSWORD", ""),
+          },
+          options: {
+            ssl: false,
+          },
+        },
       },
-      options: {
-        ssl: false,
+    };
+  } else {
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "sqlite",
+            filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+          },
+          options: {
+            useNullAsDefault: true,
+          },
+        },
       },
-    },
-  },
-});
+    };
+  }
+};
