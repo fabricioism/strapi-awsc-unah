a0eaaad2701888c0b5d923846b51de82abd0ea9d
Fabricio Murillo <fabriciomurillo24@gmail.com>
2020-12-02 19:22:48 -0600

    :rocket: Sobrecargar controlador de empleado

--- api/employee/controllers/employee.js
+++ api/employee/controllers/employee.js
@@ -1,8 +1,58 @@
-'use strict';
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**CREATE */
+  async create(ctx) {
+    let entity;
+
+    const {
+      FirstName,
+      MiddleName,
+      LastName,
+      Email,
+      PhoneNumber,
+      NationalIDNumber,
+      JobTitle,
+      BirthDate,
+      MaritalStatus,
+      Gender,
+      HireDate,
+      SalariedFlag,
+      VacationHours,
+      SickLeaveHours,
+      CurrentFlag,
+    } = ctx.request.body;
+
+    const personData = { FirstName, MiddleName, LastName, PhoneNumber, Email };
+
+    /** Inicio Creando la persona */
+    const personEntity = await strapi.services.person.create(personData);
+    /** Fin Creando la persona */
+
+    /** Creando el empleado */
+    const employeeData = {
+      NationalIDNumber,
+      JobTitle,
+      BirthDate,
+      MaritalStatus,
+      Gender,
+      HireDate,
+      SalariedFlag,
+      VacationHours,
+      SickLeaveHours,
+      CurrentFlag,
+      PersonType: "EM",
+      NameStyle: false,
+      person: personEntity.id,
+    };
+
+    entity = await strapi.services.employee.create(employeeData);
+    return sanitizeEntity(entity, { model: strapi.models.employee });
+    /** Fin Creando el empleado */
+  },
+};
